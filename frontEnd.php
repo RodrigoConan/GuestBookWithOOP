80ee7dd4d967948b0399b7a37f86e8964a7f09d4
Rodrigo Galura III <Rodrigo_Galura3rd@yahoo.com>
2013-12-04 20:53:09 +0800

    Update frontEnd.php

--- frontEnd.php
+++ frontEnd.php
@@ -40,7 +40,7 @@
 				<td>
 					<center>
 					<h2>POST NEW MESSAGE</h2>
-					<form method = "POST" action = "BackEnd.php">
+					<form method = "POST" action = "backEnd.php">
 						Name:<input type = "text" name = "name" id = "name"><br>
 						Email:&nbsp<input type = "text" name = "emailAdd" id = "emailAdd"><br>
 						Message:<br><textarea rows = "5" cols = "22" name = "message" id = "message"></textarea><br>
@@ -55,15 +55,38 @@
 
 </html>
  <script type="text/javascript">
- 	function validate(){
+ 	function validate() {
  		var name = document.getElementById("name").value;
  		var emailAdd = document.getElementById("emailAdd").value;
- 		if(name.length == "" || emailAdd.length == ""){
- 			alert("Either Name or Emailadd is Invalid");
- 			return false;
+ 		var emailAddIsAllowAt = false;
+ 		var emailAddIsAllowDot = false;
+ 		if(name.length != "" && emailAdd.length != ""){
+	 		for(i = 0; i < emailAdd.length; i++){
+	 			if(emailAdd.charAt(i) == '@'){
+	 				emailAddIsAllowAt = true;
+	 			}
+		 		if(emailAdd.charAt(i) == '.'){
+		 			emailAddIsAllowDot = true;
+		 		}
+	 		}
+
+	 		if(emailAddIsAllowAt && !emailAddIsAllowDot){
+	 			alert("Need '.' in emailadd");
+	 			return false;
+	 		}else if(!emailAddIsAllowAt && emailAddIsAllowDot){
+	 			alert("Need '@' in emailadd");
+	 			return false;
+	 		}else if(!emailAddIsAllowAt && !emailAddIsAllowDot){
+	 			alert("Check if either '@' or '.' is missing");
+	 			return false;
+	 		}else{
+	 			return true;
+	 		}
  		}else{
- 			return true;
+ 			alert("Either name or email is Invalid");
+ 			return false;
  		}
+ 		
  	}
 
  </script>
